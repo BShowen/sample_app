cb1f44f09f527d53022abba21dcd5de940d66671
Bradley Showen <BShowen@me.com>
2019-11-29 19:28:41 -0500

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "Hello, world!"
+  def home
+    render "static_pages/home"
   end
 end
