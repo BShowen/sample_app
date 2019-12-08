06cb1fd11b3a21fc4d0732579c6f7a94e2fb4728
Bradley Showen <BShowen@me.com>
2019-12-08 08:11:32 -0500

    Rename home route and add link_to in contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'application#home'
-  get 'static_pages/home'
+  get 'static_pages/home', as: :home_page #I named this route "home_page" see page 19 in Ruby Guides on Routes chapter. 
   get 'static_pages/help'
-  get 'static_pages/about'
+  get 'static_pages/about' #this url allows a url like http://localhost:3000/static_pages/about
+  # get 'about', action: :about, controller: 'static_pages' #this route allows a url like http://localhost:3000/about
   get 'static_pages/contact'
 end
