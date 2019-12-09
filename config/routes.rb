c84b72fbfe38b1baf5c1ab12cf38f2734a2225a4
Bradley Showen <BShowen@me.com>
2019-12-09 18:51:51 -0500

    Finsih layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new' 
+
   root 'application#home'
-  get 'static_pages/home', as: :home_page #I named this route "home_page" see page 19 in Ruby Guides on Routes chapter. 
-  get 'static_pages/help'
-  get 'static_pages/about' #this url allows a url like http://localhost:3000/static_pages/about
-  # get 'about', action: :about, controller: 'static_pages' #this route allows a url like http://localhost:3000/about
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about' 
+  get '/contact', to: 'static_pages#contact'
 end
+
+# get 'about', action: :about, controller: 'static_pages' #this route allows a url like http://localhost:3000/about
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper #I added this so now in my integration test I can use the application helpers that ive defined in app/helpers/application_helper.rb
 
   # Add more helper methods to be used by all tests here...
 end
