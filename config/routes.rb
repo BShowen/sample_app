d03abd5d06c6b4b8a1af51362e75747f2106d3bb
Bradley Showen <BShowen@me.com>
2019-12-24 08:50:49 -0500

    switching branches

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about' 
   get '/contact', to: 'static_pages#contact'
+
+  resources :users
 end
 
 # get 'about', action: :about, controller: 'static_pages' #this route allows a url like http://localhost:3000/about
