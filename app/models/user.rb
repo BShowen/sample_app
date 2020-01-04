9cab35d7a4ae5d0603a824c0c4372f2fbba7df51
Bradley Showen <BShowen@me.com>
2020-01-03 20:25:04 -0500

    finish user signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    before_save { self.email.downcase! }    
+    before_save do 
+        self.email.downcase! 
+        self.name.downcase!
+    end
     validates :name, presence: true, length: { maximum: 20   }
     validates :email, presence: true, length: { maximum: 244 }, 
                                 format: { with: VALID_EMAIL_REGEX },
